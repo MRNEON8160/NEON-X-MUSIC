a7b29f0fe67f55e625f60e71191629d7dbde99cc
MR NEON <148223449+MRNEON8160@users.noreply.github.com>
2023-10-31 15:19:40 +0530

    Update Config.py

--- Config.py
+++ Config.py
@@ -8,10 +8,10 @@ class Config(object):
     MANAGEMENT_MODE = os.environ.get("MANAGEMENT_MODE", True)
     HEROKU_MODE = os.environ.get("HEROKU_MODE", None)
     BOT_USERNAME = os.environ.get("BOT_USERNAME", "")
-    SUPPORT = os.environ.get("SUPPORT", "TheSupportChat") # Your Support
-    CHANNEL = os.environ.get("CHANNEL", "TheUpdatesChannel") # Your Channel
-    START_IMG = os.environ.get("START_IMG", "https://telegra.ph/file/3d8ecee0ba7dddfc6fce4.jpg")
-    CMD_IMG = os.environ.get("CMD_IMG", "https://telegra.ph/file/66518ed54301654f0b126.png")
+    SUPPORT = os.environ.get("SUPPORT", "THESUKOONLURIC") # Your Support
+    CHANNEL = os.environ.get("CHANNEL", "THESUKOONLYRIC") # Your Channel
+    START_IMG = os.environ.get("START_IMG", "https://telegra.ph/file/690915a505116fb63336a.jpg")
+    CMD_IMG = os.environ.get("CMD_IMG", "https://telegra.ph/file/690915a505116fb63336a.jpg")
     ASSISTANT_ID = int(os.environ.get("ASSISTANT_ID", "")) # telegram I'd not Username
     AUTO_LEAVE_TIME = int(os.environ.get("AUTO_LEAVE_ASSISTANT_TIME", "54000")) # in seconds
     AUTO_LEAVE = os.environ.get('AUTO_LEAVING_ASSISTANT', None) # Change it to "True"
